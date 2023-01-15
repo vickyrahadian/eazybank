3fa51638155d98087d299730aab01263948bec34
vicky <vicky.rahadian@gmail.com>
2023-01-15 13:31:08 +0700

    init project

---
+++ src/main/java/com/kode19/eazybankbackend/EazyBankBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kode19.eazybankbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EazyBankBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EazyBankBackendApplication.class, args);
+    }
+
+}
